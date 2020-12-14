99a2dfbb2db7ea90088319a3cb9505bb7cdb517e
Илья <lionson@mail.ru>
2020-12-14 20:35:21 +0200

    remove dependece between ApiEmployee and EmployeeScheme

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
-from pydantic import BaseModel
-from database.scheme import EmployeeScheme
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import  Literal
 
-# potentialy could be diffrent
-ApiEmployee = EmployeeScheme
+# same as EmployeeScheme, but potentially could be different
+class ApiEmployee(BaseModel):
+    name: str
+    email: EmailStr
+    age: int
+    company: str
+    join_date: datetime
+    job_title: str
+    gender: Literal["male", "female", "other"]
+    salary: int
