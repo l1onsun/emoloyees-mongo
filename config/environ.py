f064df0cbceb1f6487f6978216b9c689019c22dd
Илья <lionson@mail.ru>
2020-12-14 02:19:08 +0200

    configs

--- config/environ.py
+++ config/environ.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 
 from pydantic import BaseSettings, Field
-
+from typing import Optional
 
 @lru_cache
 def get_env():
@@ -11,20 +11,17 @@ def get_env():
 class EnvSettings(BaseSettings):
     allow_drop_tables: bool = Field(False, env='ALLOW_DROP_TABLES')
 
-    mongo_db: str = Field(..., env='POSTGRES_DB')
-    mongo_user: str = Field(..., env='POSTGRES_USER')
-    mongo_password: str = Field(..., env='POSTGRES_PASSWORD')
-    mongo_host: str = Field(..., env='POSTGRES_HOST')
-    mongo_port: str = Field(..., env='POSTGRES_PORT')
+    mongo_db: str
+    mongo_user: str
+    mongo_auth_db: Optional[str] = None
+    mongo_password: str
+    mongo_host: str
+    mongo_port: int
+    mongo_min_connections: int
+    mongo_max_connections: int
 
+    # mongo_seed_collection: str
+    mongo_seed_json: str
     # postgres_dsn: PostgresDsn = None
     # postgres_asyncpg_dsn: PostgresAsyncDsn = None
 
-
-class TestP(BaseSettings):
-    x: int
-    y: int
-    z: int
-
-    def __post_init__(self):
-        self.z = self.x + self.y
---
+++ config/gunicorn_conf.py
@@ -0,0 +1,33 @@
+from pydantic import BaseSettings, Field
+from typing import Optional
+import multiprocessing
+
+class GunicornSettings(BaseSettings):
+    # gunicorn
+    workers_per_core: int = Field(1, env="GUNICORN_WORKERS_PER_CORE")
+    max_workers: int = Field(2, env="GUNICORN_MAX_WORKERS")
+    web_concurrency: Optional[int] = Field(None, env="GUNICORN_WEB_CONCURRENCY")
+    host: str = Field("localhost", env="GUNICORN_HOST")
+    port: str = Field("8080", env="GUNICORN_PORT")
+    timeout: int = Field(120, env="GUNICORN_TIMEOUT")
+    keepalive: int = Field(5, env="GUNICORN_KEEP_ALIVE")
+
+gunicorn_env = GunicornSettings()
+
+def count_workers():
+    if gunicorn_env.web_concurrency:
+        workers = gunicorn_env.web_concurrency
+    else:
+        cores = multiprocessing.cpu_count()
+        default_web_concurrency = gunicorn_env.workers_per_core * cores
+        workers = min(int(default_web_concurrency), gunicorn_env.max_workers)
+
+    assert workers > 0
+    return workers
+
+
+# Gunicorn config
+bind = f"{gunicorn_env.host}:{gunicorn_env.port}"
+workers = count_workers()
+timeout = gunicorn_env.timeout
+keepalive = gunicorn_env.keepalive
